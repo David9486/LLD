deb6c3aeec2ed201663585201392536b78605c9a
David9486 <davidraja76399@gmail.com>
2025-12-31 21:05:10 +0530

    updated logic overriden methods from interface

--- Text-Editor/src/main/java/TextOperations/ConsoleLevel.java
+++ Text-Editor/src/main/java/TextOperations/ConsoleLevel.java
@@ -1,12 +1,17 @@
 package TextOperations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConsoleLevel extends TextFormat implements TextFunctions{
 
     @Override
 
+    List<String> containsWord = new ArrayList<>();
+
     public void insert(String newWord){
 
-
+        list.add()
 
     }
 
