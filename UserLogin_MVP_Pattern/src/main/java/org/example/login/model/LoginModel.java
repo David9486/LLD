9c5287ed0c53184f86e0b889dd4a1ccb61e95d6f
David9486 <davidraja76399@gmail.com>
2026-02-07 17:42:49 +0530

    completed login module using MVP pattern

--- UserLogin_MVP_Pattern/src/main/java/org/example/login/model/LoginModel.java
+++ UserLogin_MVP_Pattern/src/main/java/org/example/login/model/LoginModel.java
@@ -1,26 +1,40 @@
 package org.example.login.model;
 
+import org.example.login.presenter.LoginPresenter;
+import org.example.login.repository.UserRepoImplementation;
 import org.example.login.repository.UserRepository;
 
 public class LoginModel {
 
     private final UserRepository userRepository;
+    private final LoginPresenter loginPresenter;
 
-    public LoginModel(UserRepository userRepository){
+    public LoginModel(LoginPresenter loginPresenter){
 
-        this.userRepository = userRepository;
+        userRepository = new UserRepoImplementation();
+        this.loginPresenter = loginPresenter;
 
-    }
+    }//
 
-    public boolean validateLogin(String userName,String password){
+    public void validateLogin(String userName,String password){
 
         if(userName == null || password == null){
 
-            return false;
+            loginPresenter.loginFailed();
 
         }
 
-        return userRepository.checkLogin(userName,password);
+        else if(userRepository.checkLogin(userName,password)){
+
+            loginPresenter.loginSuccess();
+
+        }
+
+        else{
+
+            System.out.println("something went wrong");
+
+        }
 
     }
 
--- UserLogin_MVP_Pattern/src/main/java/org/example/login/presenter/LoginPresenter.java
+++ UserLogin_MVP_Pattern/src/main/java/org/example/login/presenter/LoginPresenter.java
@@ -7,32 +7,33 @@ import org.example.login.view.LoginView;
 
 public class LoginPresenter {
 
-    private LoginView loginView;
-    private LoginModel loginModel;
+    private final LoginView loginView;
+    private final LoginModel loginModel;
+
+    public void loginSuccess(){
+
+        loginView.showLoginSuccess();
+
+    }
+    public void loginFailed(){
+
+        loginView.showLoginError();
+
+    }
 
     public LoginPresenter(LoginView loginView){
 
         this.loginView = loginView;
 
-        UserRepository userRepository = new UserRepoImplementation();
+        //UserRepository userRepository = new UserRepoImplementation();
 
-        this.loginModel = new LoginModel(userRepository);
+        this.loginModel = new LoginModel(this);
 
     }
 
     public void login(String userName,String password){
 
-        if(loginModel.validateLogin(userName,password)){
-
-            loginView.showLoginSuccess();
-
-        }
-
-        else{
-
-            loginView.showLoginError();
-
-        }
+        loginModel.validateLogin(userName,password);
 
     }
 
